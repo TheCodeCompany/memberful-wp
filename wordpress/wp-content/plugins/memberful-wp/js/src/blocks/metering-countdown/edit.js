29c7c23f4a38b9d61e4b7e1901aa20cbb403060a
Anton Vanyukov <a.vanyukov@vcore.ru>
2026-05-26 07:28:14 +1000

    Allow inserting count template

--- wordpress/wp-content/plugins/memberful-wp/js/src/blocks/metering-countdown/edit.js
+++ wordpress/wp-content/plugins/memberful-wp/js/src/blocks/metering-countdown/edit.js
@@ -11,7 +11,17 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import {
+	BlockControls,
+	RichText,
+	store as blockEditorStore,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { ToolbarButton, ToolbarGroup } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { create, insert, toHTMLString } from '@wordpress/rich-text';
+
+const COUNT_PLACEHOLDER = '{count}';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -21,16 +31,83 @@ import { useBlockProps, RichText } from '@wordpress/block-editor';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
+export default function Edit( { attributes, setAttributes, clientId } ) {
 	const { template } = attributes;
+	const { selectionStart, selectionEnd } = useSelect(
+		( select ) => {
+			const { getSelectionStart, getSelectionEnd } =
+				select( blockEditorStore );
+			const start = getSelectionStart();
+			const end = getSelectionEnd();
+
+			if (
+				start.clientId !== clientId ||
+				end.clientId !== clientId ||
+				start.attributeKey !== 'template' ||
+				end.attributeKey !== 'template'
+			) {
+				return {};
+			}
+
+			return {
+				selectionStart: start.offset,
+				selectionEnd: end.offset,
+			};
+		},
+		[ clientId ]
+	);
+
+	const insertCountPlaceholder = () => {
+		const value = create( { html: template || '' } );
+		const selectedStart = Number.isInteger( selectionStart )
+			? selectionStart
+			: value.text.length;
+		const selectedEnd = Number.isInteger( selectionEnd )
+			? selectionEnd
+			: selectedStart;
+		const insertStart = Math.max(
+			0,
+			Math.min( selectedStart, selectedEnd, value.text.length )
+		);
+		const insertEnd = Math.max(
+			0,
+			Math.min( Math.max( selectedStart, selectedEnd ), value.text.length )
+		);
+		const nextValue = insert(
+			value,
+			COUNT_PLACEHOLDER,
+			insertStart,
+			insertEnd
+		);
+
+		setAttributes( {
+			template: toHTMLString( { value: nextValue } ),
+		} );
+	};
 
 	return (
-		<RichText
-			{ ...useBlockProps() }
-			tagName="p"
-			value={ template }
-			onChange={ ( value ) => setAttributes( { template: value } ) }
-			placeholder={ __( 'You have {count} free articles left.', 'memberful' ) }
-		/>
+		<>
+			<BlockControls group="block">
+				<ToolbarGroup>
+					<ToolbarButton
+						title={ __( 'Insert count placeholder', 'memberful' ) }
+						onClick={ insertCountPlaceholder }
+					>
+						{ COUNT_PLACEHOLDER }
+					</ToolbarButton>
+				</ToolbarGroup>
+			</BlockControls>
+			<RichText
+				{ ...useBlockProps() }
+				identifier="template"
+				tagName="p"
+				value={ template }
+				onChange={ ( value ) => setAttributes( { template: value } ) }
+				placeholder={ __(
+					'You have {count} free articles left.',
+					'memberful'
+				) }
+			/>
+		</>
 	);
 }
